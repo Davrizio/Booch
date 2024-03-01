042a6916ccf7d8e0603d674622b1ecff54f37586
davrizio <davriziod@gmail.com>
2024-03-01 09:58:43 -0500

    added animation to login and signup added logo to index

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,33 @@ module.exports = {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        rotationalwave: "wave 2s ease-in-out infinite",
+        fadeinup: "fadeinup 0.6s ease-in-out"
+      },
+      keyframes: {
+        wave: {
+          "0%": {transform: "rotate(0deg)"},
+          "25%": {transform: "rotate(10deg)"},
+          "50%": {transform: "rotate(-10deg)"},
+          "75%": {transform: "rotate(10deg)"},
+          "100%": {transform: "rotate(0deg)"}
+        },
+        fadeinup: {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(20px)"
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)"
+          }
+        }
+      }
+    },
   },
+  
   plugins: [
     require('flowbite/plugin'),
     require('tailwindcss-3d')
